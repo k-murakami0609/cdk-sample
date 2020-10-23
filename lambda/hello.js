b27e99989fc0bed3f0ad8fd27cf35464af8558de
k-murakami0609 <k-murakami0609@hatena.ne.jp>
2020-10-23 16:17:09 +0900

    uploadできるようにしt

--- lambda/hello.js
+++ lambda/hello.js
@@ -2,50 +2,41 @@ const chromium = require("chrome-aws-lambda");
 const aws = require("aws-sdk");
 const s3 = new aws.S3();
 const fs = require("fs");
-
-const AWSXRay = require('aws-xray-sdk-core');
+const { v4: uuidv4 } = require('uuid');
 
 exports.handler = async function (event) {
-    const seg = AWSXRay.getSegment();
-    const subsegment1 = AWSXRay.getSegment().addNewSubsegment('launch');
     const browser = await chromium.puppeteer.launch({
         args: chromium.args,
         defaultViewport: chromium.defaultViewport,
         executablePath: await chromium.executablePath,
         headless: chromium.headless,
     });
-    subsegment1.close();
 
-    const subsegment2 = AWSXRay.getSegment().addNewSubsegment('set');
     const file = fs.readFileSync(__dirname + "/screen/sample.html");
     const page = await browser.newPage();
     await page.setContent(file.toString());
-    subsegment2.close();
 
-    const subsegment3 = AWSXRay.getSegment().addNewSubsegment('screenshot');
     const screenshot = await page.screenshot({
         type: "png",
         fullPage: true,
         omitBackground: true,
         encoding: "binary",
     });
-    subsegment3.close();
-
     await browser.close();
 
-    const subsegment4 = AWSXRay.getSegment().addNewSubsegment('upload');
+    const key = uuidv4();
+    const bucket = "cdk-snapshot-sample";
     const result = await s3.upload({
         Bucket: "cdk-snapshot-sample",
-        Key: "test.png",
+        Key: `${key}.png`,
         Body: screenshot,
         ContentType: "image/png",
-    }).promise()
-    subsegment4.close();
+    }).promise();
 
     console.log("request:", JSON.stringify(event, undefined, 2));
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/plain" },
-        body: `Hello, CDK! You've hit ${event.path}\n`
+        body: `${bucket}/${key}.png`
     };
 };
